bbe938e8d1d747834c67be0a7a49df541574a23f
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2025-01-09 22:07:46 -0300

    Changes to the dark mode.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import './index.css'
-import App from './App.tsx'
-import * as ReactDOM from 'react-dom/client'
-import { NextUIProvider } from '@nextui-org/react'
+import './index.css';
+import App from './App.tsx';
+import * as ReactDOM from 'react-dom/client';
+import { NextUIProvider } from '@nextui-org/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<NextUIProvider>
-		<div className='dark'>
+		<div className='dark text-foreground bg-background'>
 			<App />
 		</div>
 	</NextUIProvider>
