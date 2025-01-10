5d53ecac3646dce0834f8eaf8ada208294872e8f
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2025-01-09 22:05:08 -0300

    Updated libraries and configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+
+	server: {
+		host: '0.0.0.0', // Listen on all IP addresses, making it accessible on the LAN
+		port: 5174       // You can change the port number if you want
+	}
+
 })
