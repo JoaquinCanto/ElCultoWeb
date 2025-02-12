90a957ae4de3a711bd3e0e0d4d535711ffbd13c2
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2025-02-11 23:27:21 -0300

    Updated Routes.

--- src/models/Routes.tsx
+++ src/models/Routes.tsx
@@ -2,8 +2,12 @@ export enum PublicRoutes {
 	HOME = "/",
 
 	MESAS = "/mesas",
+	REGISTRARSE = "/registrarse",
+	INGRESAR = "/ingresar",
 }
 
 export enum PrivateRoutes {
+	MISMESAS = "/mismesas",
+	ADMINISTRACION = "/administracion",
 
 }
