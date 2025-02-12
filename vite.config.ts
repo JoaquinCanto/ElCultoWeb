35db5db33f4ab29b4f52635db2a71e653fe3100f
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2025-02-11 21:52:11 -0300

    Updated vite config for .env .

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+	const env = loadEnv(mode, process.cwd());
+	return {
+		plugins: [react()],
 
-	server: {
-		host: '0.0.0.0', // Listen on all IP addresses, making it accessible on the LAN
-		port: 5174       // You can change the port number if you want
+		define: {
+			__APP_ENV__: JSON.stringify(env.APP_ENV),
+		},
+
+		server: {
+			host: '0.0.0.0', // Listen on all IP addresses, making it accessible on the LAN
+			port: 5174       // You can change the port number if you want
+		},
 	}
-
 })
