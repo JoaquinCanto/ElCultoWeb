ee56530ceba5681d9de6454dcd3333f8d5eda02b
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2025-02-11 23:38:18 -0300

    Added routes REGISTRARSE, INGRESAR, MISMESAS and ADMINISTRACION.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
-import { PublicRoutes } from './models/Routes';
+import { PrivateRoutes, PublicRoutes } from './models/Routes';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthGuard } from './guards/authGuard';
 
+// import Placeholder from './components/Placeholder';
 import Layout from './pages/Layout';
-import Placeholder from './components/Placeholder';
 import Boards from './pages/Boards';
+import Register from './pages/Register';
+import LogIn from './pages/LogIn';
+import MyBoards from './pages/MyBoards';
+import ControlPanel from './pages/ControlPanel';
 
 function App() {
 
@@ -13,8 +18,15 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route element={<Layout />} >
-						<Route path={PublicRoutes.HOME} element={<Placeholder />} />
+						<Route path={PublicRoutes.HOME} element={<Boards />} />
 						<Route path={PublicRoutes.MESAS} element={<Boards />} />
+						<Route path={PublicRoutes.REGISTRARSE} element={<Register />} />
+						<Route path={PublicRoutes.INGRESAR} element={<LogIn />} />
+
+						<Route element={<AuthGuard />}>
+							<Route path={PrivateRoutes.MISMESAS} element={<MyBoards />} />
+							<Route path={PrivateRoutes.ADMINISTRACION} element={<ControlPanel />} />
+						</Route>
 					</Route>
 				</Routes>
 			</BrowserRouter>
