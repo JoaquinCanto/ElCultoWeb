dc7e5a53fa76717b9683094eb604702035acbc4a
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2025-02-11 23:22:06 -0300

    Updated Board page.

--- src/pages/Boards.tsx
+++ src/pages/Boards.tsx
@@ -1,71 +1,42 @@
-import axios from "axios";
+import { useState } from "react";
 import Game from "../components/Game";
-import { useEffect, useState } from "react";
-import { Button, Card, Skeleton, Divider } from "@heroui/react";
-import usePersonaStore from "../stores/personaStore";
 import { FaPlus } from "react-icons/fa6";
-import ModalNewMesa from "../components/ModalNewBoard";
-
-interface BoardsTypes {
-	idMesa: number,
-	juego: any,
-	narrador: any,
-	fechaHora: string,
-	lugar: any,
-	descripcion: any,
-	notas: string,
-	cupoMin: number,
-	cupoMax: number
-	jugadores: any
-}
+import { BoardGet } from "../types/board";
+import { useOpenBoards } from "../services/queries";
+import usePersonStore from "../stores/personStore";
+import ModalBoardData from "../components/ModalBoardData";
+import { Button, Card, Skeleton, Divider } from "@heroui/react";
 
 export default function Boards() {
 
-	const { tipo } = usePersonaStore();
-
-	const [boards, setBoards] = useState([]);
-	const [isLoaded, setIsLoaded] = useState(false);
+	const { tipo } = usePersonStore();
 
 	const [isModalOpen, setIsModalOpen] = useState(false);
 	const toggleModal = () => setIsModalOpen(!isModalOpen);
 
-	const fetchBoards = async () => {
-		axios
-			.get("http://localhost:3000/mesa/abierta")
-			.then((response) => {
-				setBoards(response.data.items);
-				// console.log(response.data.items);
-				setIsLoaded(true);
-			})
-			.catch((error) => {
-				console.error('Error fetching data:', error);
-			});
-	};
-
-	useEffect(() => {
-		fetchBoards();
-	}, [])
+	const openBoardsQuery = useOpenBoards();
 
 	const renderBoards = () => {
-		if (boards.length < 1) {
+		console.log(openBoardsQuery.data);
+		if (!openBoardsQuery.data?.items || !Array.isArray(openBoardsQuery.data.items)) {
 			return <p>No hay mesas disponibles</p>
 		}
 		else {
-			return boards.map((board: BoardsTypes) => (
+			return openBoardsQuery.data.items.map((board: BoardGet) => (
 				<Game
 					key={board.idMesa}
 					idMesa={board.idMesa}
 					juego={board.juego.nombre}
 					narrador={board.narrador.apodo}
 					fecha={board.fechaHora}
-					lugar={board.lugar.nombre}
+					lugar={board.lugar}
 					descripcion={board.juego.descripcion}
 					notas={board.notas}
 					minJugadores={board.cupoMin}
 					maxJugadores={board.cupoMax}
 					cantJugadores={board.jugadores.length}
 					inscriptos={board.jugadores}
-					updateMesas={fetchBoards}
+					updateMesas={openBoardsQuery.refetch}
 				/>
 			))
 		}
@@ -74,8 +45,9 @@ export default function Boards() {
 	function renderNewBoardButton() {
 		if (tipo === "Narrador") {
 			return (
-				<>
-					<Button color='primary'
+				<div className="flex flex-col gap-2">
+					<Button className="w-40"
+						color='primary'
 						variant='ghost'
 						startContent={<FaPlus />}
 						onPress={toggleModal}
@@ -83,7 +55,7 @@ export default function Boards() {
 						Agregar Mesa
 					</Button>
 					<Divider className="w-screen" />
-				</>
+				</div>
 			)
 		}
 	}
@@ -99,23 +71,23 @@ export default function Boards() {
 
 	return (
 		<div className='h-full p-4 flex flex-wrap gap-4'>
-			{isLoaded ?
+			{!openBoardsQuery.isPending ?
 				renderPage()
 				:
 				<>
 					<div className="flex flex-col gap-3">
 						<Card className="w-[200px] space-y-5 p-4" radius="lg">
-							<Skeleton className="rounded-lg" isLoaded={isLoaded}>
+							<Skeleton className="rounded-lg" >
 								<div className="h-24 rounded-lg bg-secondary" />
 							</Skeleton>
 							<div className="space-y-3">
-								<Skeleton className="w-3/5 rounded-lg" isLoaded={isLoaded}>
+								<Skeleton className="w-3/5 rounded-lg" >
 									<div className="h-3 w-full rounded-lg bg-secondary" />
 								</Skeleton>
-								<Skeleton className="w-4/5 rounded-lg" isLoaded={isLoaded}>
+								<Skeleton className="w-4/5 rounded-lg" >
 									<div className="h-3 w-full rounded-lg bg-secondary-300" />
 								</Skeleton>
-								<Skeleton className="w-2/5 rounded-lg" isLoaded={isLoaded}>
+								<Skeleton className="w-2/5 rounded-lg" >
 									<div className="h-3 w-full rounded-lg bg-secondary-200" />
 								</Skeleton>
 							</div>
@@ -123,14 +95,9 @@ export default function Boards() {
 					</div>
 				</>
 			}
-			<ModalNewMesa
+			<ModalBoardData
 				isOpen={isModalOpen}
 				onOpenChange={toggleModal}
-				boards={boards}
-				newBoardAdded={() => {
-					fetchBoards(); // Refresh the boards list
-					setIsModalOpen(false); // Close the modal
-				}}
 			/>
 		</div>
 	)
