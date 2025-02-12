5e9d08e9948dfba408441956cb1af98ec7f1e001
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2025-02-11 23:36:14 -0300

    Added query client and locale es-ES.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import './index.css';
 import App from './App.tsx';
 import * as ReactDOM from 'react-dom/client';
-import { NextUIProvider } from '@nextui-org/react';
+import { HeroUIProvider } from "@heroui/react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+	defaultOptions: { queries: { retry: 5, retryDelay: 1000 } },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<NextUIProvider>
-		<div className='dark text-foreground bg-background'>
+	<QueryClientProvider client={queryClient}>
+		<HeroUIProvider
+			locale="es-ES"
+			className='dark text-foreground bg-background'
+		>
 			<App />
-		</div>
-	</NextUIProvider>
-)
+		</HeroUIProvider>
+	</QueryClientProvider>
+);
