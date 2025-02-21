5fdbdce2d9abef955ae52db65ffb5577607cf221
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2025-02-21 01:55:49 -0300

    Cleaning.

--- src/pages/Boards.tsx
+++ src/pages/Boards.tsx
@@ -43,7 +43,7 @@ export default function Boards() {
 	}
 
 	function renderNewBoardButton() {
-		if (tipo === "Narrador") {
+		if (tipo !== "Jugador") {
 			return (
 				<div className="flex flex-col gap-2">
 					<Button className="w-40"
