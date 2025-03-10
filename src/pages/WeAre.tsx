5ce5411ce408412fabfd479b460e1e9a2fcf27bf
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2025-03-10 16:57:43 -0300

    Edited WeAre page.

--- src/pages/WeAre.tsx
+++ src/pages/WeAre.tsx
@@ -1,33 +1,50 @@
-
-
 const WeAre = () => {
 	return (
-		<div className="flex flex-col  gap-1 p-4 max-w-3xl">
-			<p>
-				Les contamos un poco de nosotros...
-			</p>
-			<p>Misión:</p>
-			<p>
-				Somos un grupo de frikis interesados en el rol y la narrativa con la intención de trabajar estos aspectos y acrecentar nuestros conocimientos y experiencias en este bello mundo de las historias construidas en conjunto.
-			</p>
-			<p>
-				Visión:
-				Apuntamos a tener un amplio conocimiento de los sistemas de rol para que tanto jugadores como narradores o futuros narradores tengan una grata experiencia.
-			</p>
-			<p>
-				Objetivos:
-				🎲Iniciar a jugadores comprometidos en el mundo del rol.
-				🎲Brindar un espacio seguro para que los jugadores tengan sus primeras experiencias como narradores.
-				🎲Probar sistemas nuevos y diferentes que enriquezcan las experiencias narrativas.
-				🎲Aprender a desarrollar personajes, pensando en los conceptos y luego adaptando los sistemas.
-			</p>
-
-			<p>
-				Normas e información:
-				🎲 Somos un grupo respetuoso de amigos que pretenden aprender juntos y enseñar a los demás sin que se convierta en una obligación o una carga, es por ello que las mesas abiertas a la gente externa son mensuales.
-				🎲 No toleramos la intolerancia ni los malos tratos, todos podemos tener una opinión siempre y cuando sea constructiva y respetuosa, la idea es tirar todos hacia el mismo lugar.
-				🎲 Este se pretende ser un espacio seguro por lo que nos reservamos el derecho de admisión.
-			</p>
+		<div className="flex justify-center items-center min-w-screen">
+			<div className="flex flex-col items-center gap-3 p-4 max-w-3xl text-justify">
+				<h1 className="font-bold text-2xl">
+					Les contamos un poco de nosotros...
+				</h1>
+				<div>
+					<h2 className="font-bold text-xl">Misión:</h2>
+					<p>
+						Somos un grupo de frikis interesados en el rol y la narrativa con la intención de trabajar estos aspectos y acrecentar nuestros conocimientos y experiencias en este bello mundo de las historias construidas en conjunto.
+					</p>
+				</div>
+				<div>
+					<h2 className="font-bold text-xl">Visión:</h2>
+					<p>
+						Apuntamos a tener un amplio conocimiento de los sistemas de rol para que tanto jugadores como narradores o futuros narradores tengan una grata experiencia.
+					</p>
+				</div>
+				<div>
+					<h2 className="font-bold text-xl">Objetivos:</h2>
+					<p>
+						🎲Iniciar a jugadores comprometidos en el mundo del rol.
+					</p>
+					<p>
+						🎲Brindar un espacio seguro para que los jugadores tengan sus primeras experiencias como narradores.
+					</p>
+					<p>
+						🎲Probar sistemas nuevos y diferentes que enriquezcan las experiencias narrativas.
+					</p>
+					<p>
+						🎲Aprender a desarrollar personajes, pensando en los conceptos y luego adaptando los sistemas.
+					</p>
+				</div>
+				<div>
+					<h2 className="font-bold text-xl">Normas e información:</h2>
+					<p>
+						🎲 Somos un grupo respetuoso de amigos que pretenden aprender juntos y enseñar a los demás sin que se convierta en una obligación o una carga, es por ello que las mesas abiertas a la gente externa son mensuales.
+					</p>
+					<p>
+						🎲 No toleramos la intolerancia ni los malos tratos, todos podemos tener una opinión siempre y cuando sea constructiva y respetuosa, la idea es tirar todos hacia el mismo lugar.
+					</p>
+					<p>
+						🎲 Este se pretende ser un espacio seguro por lo que nos reservamos el derecho de admisión.
+					</p>
+				</div>
+			</div>
 		</div>
 	)
 }
