b25a41813871cbb3364edb07a1dbc51ca4935594
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2025-03-10 16:57:14 -0300

    Added FAQ page.

---
+++ src/pages/FAQ.tsx
@@ -0,0 +1,49 @@
+
+
+const FAQ = () => {
+	return (
+		<div className="flex justify-center items-center min-w-screen">
+			<div className="flex flex-col items-center gap-3 p-4 max-w-3xl text-justify">
+				<div>
+					<h1 className="font-bold text-xl">Normas del grupo ¿Por qué estas normas?</h1>
+					<p>El Culto comenzó como un grupo de amigues y, poco a poco, se fueron integrando más personas. Como queremos continuar creciendo creemos que es necesario dejar en claro ciertos temas para asegurar un ambiente seguro para todes les integrantes.
+					</p>
+					<p>Creamos estas normas para sostener nuestro compromiso de garantizar una comunidad libre de conductas hostiles y de toda forma de discriminación (por género, nacionalidad, color, orientación sexual, etc) Apuntamos a prevenir comportamientos que impliquen acoso bajo cualquier modalidad, y generar un ambiente en el cual todas las personas se sientan cómodas y bienvenidas.
+					</p>
+				</div>
+				<div className="flex flex-col">
+					<h1 className="font-bold text-xl">Mandatos Cultistas</h1>
+					<div className="flex flex-col gap-2">
+						<p>Queremos dejar en claro que este es un ambiente feminista y que apoya a las disidencias. El machismo no tiene cabida, las joditas no serán festejadas.
+						</p>
+						<p><strong>Respeto:</strong> No se aceptan agresiones o discriminacion a miembros del grupo por los motivos anteriormente expuestos. No toleraremos comentarios discriminatorios de ninguna índole ni comportamientos de acoso.
+						</p>
+						<p><strong>Contenido seguro:</strong> No está permitido ningún tipo de contenido violento, pornográfico ni que incite a la discriminacion, marginación o el odio. No vamos a prohibir temas específicos (fútbol, política, etc) siempre y cuando nos manejemos adecuadamente sin faltas de respeto teniendo en cuenta que todos somos diferentes y podemos tener opiniones desiguales.
+						</p>
+						<p><strong>Buen Trato:</strong> Entendemos que todos podemos tener un mal día pero ninguno merece ser tratado de mala manera por lo que les pedimos que se mantenga el trato cordial en todo momento.
+						</p>
+						<p><strong>Uso adecuado de canales/chats/grupos:</strong> Evitar hacer spam en los grupos/chats/canales y usarlos con responsabilidad para los propósitos con los que fueron creados para no generar molestias a quienes les prestan atención.
+						</p>
+						<p><strong>Las Deidades tienen la última palabra:</strong> Si un miembro del Staff te pide parar de hacer algo que causa problemas para la comunidad y no acatas las normas serás expulsado o muteado del servidor/chat y de la vida. Si te haces el vivo sale macumba.
+						</p>
+					</div>
+				</div>
+				<div className="flex flex-col">
+					<h1 className="font-bold text-xl">Tu compromiso</h1>
+					<div className="flex flex-col gap-2">
+						<p>Es importante que quienes forman parte del grupo (tanto narradores del Culto, jugadores y narradores independientes) contribuyan activamente a generar un clima de cuidado y respeto, tanto dentro del grupo como fuera de él.
+						</p>
+						<p>Por este motivo invitamos a que cada persona tenga la confianza de que si está atravesando una situación de violencia o acoso, pueda acudir a alguno de los admin para hablar sobre el tema.
+						</p>
+						<p><strong>IMPORTANTE:</strong> Cuando existan problemas/diferencias personales entre dos o más personas, es responsabilidad absoluta de esas personas trabajar en sus diferencias sin afectar la dinámica del grupo. Los admin pueden mediar entre las partes pero bajo ninguna circunstancia el resto del grupo debería sentirse obligado a tomar partido por uno u otro.
+						</p>
+					</div>
+				</div>
+				<h1 className="font-bold text-xl">NOS RESERVAMOS EL DERECHO DE ADMISIÓN Y PERMANENCIA.</h1>
+				<p className="text-sm self-end">Última modificación Sept. 2022</p>
+			</div>
+		</div >
+	)
+}
+
+export default FAQ
